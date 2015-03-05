faa071f78f4be9e0a3807b48c1d6ae75e604bcf5
John Tramm <john.tramm@gmail.com>
2015-03-04 22:52:03 -0600

    cleaned up io

--- main.c
+++ main.c
@@ -10,28 +10,45 @@ double complex FNF( double complex Z );
 
 int main(void)
 {
-	long n = 100000000;
+	long n = 10000000;
 	unsigned seed = time(NULL) + 1337;
 	complex double ctr = 0;
-
+	double scale = 10.0;
+	
 	double start = omp_get_wtime();
 	long hit = 0;
+	// FNF
 	for( long i = 0; i < n; i++ )
 	{
-		double real = 1.0* (double) rand_r(&seed) / RAND_MAX;
-		double imag = 1.0 *(double) rand_r(&seed) / RAND_MAX;
+		double real = scale* (double) rand_r(&seed) / RAND_MAX;
+		double imag = scale *(double) rand_r(&seed) / RAND_MAX;
 		double complex A = real + imag*I;
 		if( cabs(A) <= 6.0 )
 			hit++;
 		complex double result = FNF(A);
-		//complex double result = Faddeeva_w(A,0.0);
 		ctr += result;
 	}
 	double end = omp_get_wtime();
-	printf("TIME = %lf seconds\n", end-start);
-	printf("%lf us per evaluation\n", (end-start) / n *1e6);
+	double FNF_time = end-start;
+	start = end;
+	// MIT
+	for( long i = 0; i < n; i++ )
+	{
+		double real = scale* (double) rand_r(&seed) / RAND_MAX;
+		double imag = scale *(double) rand_r(&seed) / RAND_MAX;
+		double complex A = real + imag*I;
+		if( cabs(A) <= 6.0 )
+			hit++;
+		complex double result = Faddeeva_w(A,0.0);
+		ctr += result;
+	}
+	end = omp_get_wtime();
+	double MIT_time = end-start;
+
+	printf("FNF avg. W evaluation time: %6.3lf (us)\n", FNF_time / n *1e6);
+	printf("MIT avg. W evaluation time: %6.3lf (us)\n", MIT_time / n *1e6);
 	printf("result: %.2lf + %.2lfi\n", creal(ctr), cimag(ctr));
-	printf("Percent Full Faddeeva Rate: %.4lf%%\n", (double) hit / n * 100.0);
+	printf("Percent Full Faddeeva Rate: %.4lf%%\n", (double) hit / (2*n) * 100.0);
 
 	return 0;
 }
