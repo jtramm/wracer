28f198c80f36bfda623909d2d42d38a37429ca44
John Tramm <john.tramm@gmail.com>
2015-03-04 23:18:06 -0600

    added realistic domain sampling

--- main.c
+++ main.c
@@ -10,18 +10,22 @@ double complex FNF( double complex Z );
 
 int main(void)
 {
-	long n = 10000000;
+	long n = 30000000;
 	unsigned seed = time(NULL) + 1337;
 	complex double ctr = 0;
 	double scale = 10.0;
+	double min_real = -9.999980e+03;
+	double max_real = 9.984410e+01;
+	double min_img = 3.461271e-01;
+	double max_img = 6.752828e-01;
 	
 	double start = omp_get_wtime();
 	long hit = 0;
 	// FNF
 	for( long i = 0; i < n; i++ )
 	{
-		double real = scale* (double) rand_r(&seed) / RAND_MAX;
-		double imag = scale *(double) rand_r(&seed) / RAND_MAX;
+		double real = min_real + (max_real - min_real) * (double) rand_r(&seed) / RAND_MAX;
+		double imag = min_img + (max_img - min_img) * (double) rand_r(&seed) / RAND_MAX;
 		double complex A = real + imag*I;
 		if( cabs(A) <= 6.0 )
 			hit++;
@@ -30,16 +34,16 @@ int main(void)
 	}
 	double end = omp_get_wtime();
 	double FNF_time = end-start;
-	start = end;
+	start = omp_get_wtime();
 	// MIT
 	for( long i = 0; i < n; i++ )
 	{
-		double real = scale* (double) rand_r(&seed) / RAND_MAX;
-		double imag = scale *(double) rand_r(&seed) / RAND_MAX;
+		double real = min_real + (max_real - min_real) * (double) rand_r(&seed) / RAND_MAX;
+		double imag = min_img + (max_img - min_img) * (double) rand_r(&seed) / RAND_MAX;
 		double complex A = real + imag*I;
 		if( cabs(A) <= 6.0 )
 			hit++;
-		complex double result = Faddeeva_w(A,0.0);
+		complex double result = Faddeeva_w(A,0.01);
 		ctr += result;
 	}
 	end = omp_get_wtime();
